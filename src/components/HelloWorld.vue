8c0487dcf7ef62cd8586c94fe2193d73313face7
Martin <martin_bjerkeengen@hotmail.com>
2019-03-26 13:46:25 +0100

    added 5 day forecasts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,19 +5,24 @@
     <h3>Current Weather</h3>
     <ul>
       <li>
-          Location: {{ info.data.location.name }} 
+          Location: {{ infoCurrent.data.location.name }} 
       </li>
       <li>
-          Weather: {{ info.data.current.condition.text }}
+          Weather: {{ infoCurrent.data.current.condition.text }}
       </li>
     </ul>
+    <h3>Forecast for {{infoCurrent.data.location.name}} </h3>
+    <li v-for="day in infoForecast.data.forecast.forecastday">
+        {{day.date}}
+        {{day.day.condition.text}}
+      </li>
     <h3>Cities</h3>
     <ul>
-      <li><a href="#" v-on:click="updateWeather('Oslo')">Oslo</a></li>
-      <li><a href="#" v-on:click="updateWeather('Bergen')">Bergen</a></li>
-      <li><a href="#" v-on:click="updateWeather('Stavanger')">Stavanger</a></li>
-      <li><a href="#" v-on:click="updateWeather('Trondheim')">Trondheim</a></li>
-      <li><a href="#" v-on:click="updateWeather('Kristiansand')">Kristiansand</a></li>
+      <li><a href="#" v-on:click="updateWeather('Oslo'), updateForecast('Oslo')">Oslo</a></li>
+      <li><a href="#" v-on:click="updateWeather('Bergen'), updateForecast('Bergen')">Bergen</a></li>
+      <li><a href="#" v-on:click="updateWeather('Stavanger'), updateForecast('Stavanger')">Stavanger</a></li>
+      <li><a href="#" v-on:click="updateWeather('Trondheim'), updateForecast('Trondheim')">Trondheim</a></li>
+      <li><a href="#" v-on:click="updateWeather('Kristiansand'), updateForecast('Kristiansand')">Kristiansand</a></li>
     </ul>
     
   </div>
@@ -29,23 +34,40 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String,
-    info: null,
+    infoCurrent: null,
+    infoForecast: null,
     base: 'Oslo',
-    url: ""
+    fore: 'Oslo',
+    urlCurrent: "",
+    urlForecast: ""
   },
   created () {
-        this.url = 'http://api.apixu.com/v1/current.json?key=d669f00ce9574e84ad695232192603&q=Oslo';
+        this.urlCurrent = 'http://api.apixu.com/v1/current.json?key=d669f00ce9574e84ad695232192603&q=Oslo';
       axios
-        .get(this.url)
-        .then(response => (this.info = response))
+        .get(this.urlCurrent)
+        .then(response => (this.infoCurrent = response))
     },
+  mounted () {
+    this.urlForecast = 'http://api.apixu.com/v1/forecast.json?key=d669f00ce9574e84ad695232192603&q=Oslo&days=5';
+        axios
+          .get(this.urlForecast)
+          .then(response => (this.infoForecast = response))
+  },
     methods: {
       updateWeather(City){
         this.base = City
-        this.url = 'http://api.apixu.com/v1/current.json?key=d669f00ce9574e84ad695232192603&q=' + this.base;
+        this.urlCurrent = 'http://api.apixu.com/v1/current.json?key=d669f00ce9574e84ad695232192603&q=' + this.base;
         axios
-          .get(this.url)
-          .then(response => (this.info = response))
+          .get(this.urlCurrent)
+          .then(response => (this.infoCurrent = response))
+      },
+      updateForecast(City){
+        this.fore = City
+        this.urlForecast = 'http://api.apixu.com/v1/forecast.json?key=d669f00ce9574e84ad695232192603&q=' + this.fore + '&days=5';
+        axios
+          .get(this.urlForecast)
+          .then(response => (this.infoForecast = response))
+
       }
     }
 }
